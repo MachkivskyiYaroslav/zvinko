36ee7d6221e191217e71b16b7920731dd3c1c431
MachkivskyiYaroslav <machkyvskiy@outlook.com>
2019-10-09 15:03:44 +0300

    new

--- controllers/slider/deletAll.js
+++ controllers/slider/deletAll.js
@@ -1,7 +1,7 @@
 const slider= require('../../dataBase/models/sliderModel');
 module.exports= async (req,res) => {
     try {
-        await slider.dropCollection((error,succesfull) => {
+        await slider.remove({},(error,succesfull) => {
             res.status(200).send(succesfull);
             console.log('collection drop');
         })
