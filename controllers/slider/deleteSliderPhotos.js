0033db02f53508bee29541c208e754066868cfc0
MachkivskyiYaroslav <machkyvskiy@outlook.com>
2019-10-15 23:09:38 +0300

    new

--- controllers/slider/deleteSliderPhotos.js
+++ controllers/slider/deleteSliderPhotos.js
@@ -15,22 +15,21 @@ module.exports = async (req, res, next) => {
             if (err) {
                 console.log(err)
             } else {
-                photoInfo=result;
+                photoInfo = result;
                 console.log(photoInfo);
                 console.log(photoInfo.url);
-                res.status(200).send(photoInfo);
                 fileDelete(photoInfo.url);
             }
         })
             .then(slider.deleteOne({_id: mongoose.Types.ObjectId(photoInfo._id)}, (err, result) => {
-            if (err) {
-                console.log(err)
-            } else {
-                console.log(result);
-                res.status(200).send(result);
-            }
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log(result);
+                    res.status(200).send(result);
+                }
 
-        }));
+            }));
     } catch (e) {
         console.log(e);
         next(new ControllerError(e.message, e.status, 'deleteSliderPhotos'));
